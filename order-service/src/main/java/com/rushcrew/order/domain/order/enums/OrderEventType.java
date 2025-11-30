2581aa229a238d276548b6239a776cfe1386845f
chachohee <cxoxee@gmail.com>
2025-11-30 16:03:42 +0900

    feat : 주문 수정(포인트 사용량 수정) 이벤트 타입 추가

--- order-service/src/main/java/com/rushcrew/order/domain/order/enums/OrderEventType.java
+++ order-service/src/main/java/com/rushcrew/order/domain/order/enums/OrderEventType.java
@@ -10,7 +10,8 @@ public enum OrderEventType {
 	PURCHASE_CONFIRMED("구매 확정"),
 	CANCELLED_BEFORE_PAYMENT("결제 전 취소"),
 	CANCELLED_AFTER_PAYMENT("결제 후 취소"),
-	REFUNDED("구매확정 후 환불");
+	REFUNDED("구매확정 후 환불"),
+	POINT_USAGE_UPDATED("포인트 사용량 수정");
 
 	private final String description;
 
