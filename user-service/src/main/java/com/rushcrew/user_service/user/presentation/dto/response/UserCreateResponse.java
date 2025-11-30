2e7e43697547c54fcfb8163ba3c56cfde1b5abc7
bbangjae <thebyunjae@naver.com>
2025-11-30 23:31:57 +0900

    Feat(user): User 회원가입 DTO 추가

---
+++ user-service/src/main/java/com/rushcrew/user_service/user/presentation/dto/response/UserCreateResponse.java
@@ -0,0 +1,19 @@
+package com.rushcrew.user_service.user.presentation.dto.response;
+
+import com.rushcrew.user_service.user.application.result.UserCreateResult;
+
+public record UserCreateResponse(
+    Long userId,
+    String email,
+    String name,
+    String role
+) {
+    public static UserCreateResponse from(UserCreateResult result) {
+        return new UserCreateResponse(
+            result.userId(),
+            result.email(),
+            result.name(),
+            result.role()
+        );
+    }
+}
