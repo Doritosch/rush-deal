9aa100d8e67db4fb16b91bc8f63b4c5c819aa3a5
bbangjae <thebyunjae@naver.com>
2025-11-30 23:33:37 +0900

    Feat(user): User 회원가입 API 구현

---
+++ user-service/src/main/java/com/rushcrew/user_service/user/presentation/UserController.java
@@ -0,0 +1,33 @@
+package com.rushcrew.user_service.user.presentation;
+
+import com.rushcrew.user_service.user.application.UserService;
+import com.rushcrew.user_service.user.application.command.UserCreateCommand;
+import com.rushcrew.user_service.user.application.result.UserCreateResult;
+import com.rushcrew.user_service.user.presentation.dto.request.UserCreateRequest;
+import com.rushcrew.user_service.user.presentation.dto.response.UserCreateResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserCreateResponse> signUp(
+        @Valid @RequestBody UserCreateRequest request
+    ) {
+        UserCreateCommand command = request.toCommand();
+
+        UserCreateResult result = userService.signUp(command);
+
+        return ResponseEntity.ok(UserCreateResponse.from(result));
+    }
+}
