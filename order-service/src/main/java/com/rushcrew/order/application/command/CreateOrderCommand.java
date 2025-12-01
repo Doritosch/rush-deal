3e56de938f137770889cea23bd7746d450e9a731
chachohee <cxoxee@gmail.com>
2025-12-02 00:13:01 +0900

    feat(order): 주문 생성 command 작성

---
+++ order-service/src/main/java/com/rushcrew/order/application/command/CreateOrderCommand.java
@@ -0,0 +1,30 @@
+package com.rushcrew.order.application.command;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.rushcrew.order.domain.vo.ShippingInfo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderCommand {
+
+	private Long userId;
+	private String timeDealId;
+	private List<OrderItemCommand> orderItemList;
+	private BigDecimal pointUsed;
+	private ShippingInfo shippingInfo;
+
+	@Getter
+	@Builder
+	@AllArgsConstructor
+	public static class OrderItemCommand {
+		private String timeDealStockId;
+		private Integer quantity;
+	}
+}
