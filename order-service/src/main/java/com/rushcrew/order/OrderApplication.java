6e8043edb99c2e6c834178d3dab6bc7a13a9d007
chachohee <cxoxee@gmail.com>
2025-12-01 20:23:36 +0900

    Chore: 공통 모듈 적용

--- order-service/src/main/java/com/rushcrew/order/OrderApplication.java
+++ order-service/src/main/java/com/rushcrew/order/OrderApplication.java
@@ -2,10 +2,20 @@ package com.rushcrew.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = {
+	"com.rushcrew.order",
+	"com.rushcrew.common"
+})
+@EntityScan(basePackages = {
+	"com.rushcrew.queue",
+	"com.rushcrew.common"
+})
 public class OrderApplication {
 
 	public static void main(String[] args) {
