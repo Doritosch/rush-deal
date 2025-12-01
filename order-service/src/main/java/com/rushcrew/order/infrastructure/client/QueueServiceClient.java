f218a36e5c85ed3dfd4cc63357320669939f72da
chachohee <cxoxee@gmail.com>
2025-12-01 18:47:17 +0900

    Feat: 대기열 외부 서비스 구현

--- order-service/src/main/java/com/rushcrew/order/infrastructure/client/QueueServiceClient.java
+++ order-service/src/main/java/com/rushcrew/order/infrastructure/client/QueueServiceClient.java
@@ -1,4 +1,13 @@
 package com.rushcrew.order.infrastructure.client;
 
-public class QueueServiceClient {
+public interface QueueServiceClient {
+
+	// 대기열 토큰 유효성 검증
+	boolean validateQueueToken(String timeDealId, Long userId);
+
+	// 대기열 토큰 TTL 연장
+	void extendTokenTtl(String timeDealId, Long userId, int seconds);
+
+	// 사용자 대기열에서 제거
+	void removeUserToken(String timeDealId, Long userId);
 }
