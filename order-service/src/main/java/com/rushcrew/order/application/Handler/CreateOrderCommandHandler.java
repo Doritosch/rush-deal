83a4565b1af457a7b33fe2db3935569356384b40
chachohee <cxoxee@gmail.com>
2025-12-02 12:02:42 +0900

    feat(order): 주문 생성 서비스 구현

---
+++ order-service/src/main/java/com/rushcrew/order/application/Handler/CreateOrderCommandHandler.java
@@ -0,0 +1,19 @@
+package com.rushcrew.order.application.Handler;
+
+import org.springframework.stereotype.Component;
+
+import com.rushcrew.order.application.command.CreateOrderCommand;
+import com.rushcrew.order.application.command.CreateOrderResult;
+import com.rushcrew.order.application.service.OrderCreationService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CreateOrderCommandHandler {
+	private final OrderCreationService orderCreationService;
+
+	public CreateOrderResult handle(CreateOrderCommand command) {
+		return orderCreationService.createOrder(command);
+	}
+}
