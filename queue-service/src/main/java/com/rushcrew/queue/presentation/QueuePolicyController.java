eb7c0ecd7c2d365b43b9ddd66710a2f869f4e639
songkyung <min.songkyung@gmail.com>
2025-12-02 14:26:48 +0900

    feature: 타임딜 정책 목록 조회 엔드포인트, Request, Command 정의

--- queue-service/src/main/java/com/rushcrew/queue/presentation/QueuePolicyController.java
+++ queue-service/src/main/java/com/rushcrew/queue/presentation/QueuePolicyController.java
@@ -2,14 +2,21 @@ package com.rushcrew.queue.presentation;
 
 import com.rushcrew.common.dto.ApiResponse;
 import com.rushcrew.queue.application.command.CreatePolicyCommand;
+import com.rushcrew.queue.application.command.SearchPolicyCommand;
+import com.rushcrew.queue.application.dto.PageQuery;
+import com.rushcrew.queue.domain.enums.QueuePolicyStatus;
 import com.rushcrew.queue.presentation.dto.request.CreatePolicyRequest;
 import com.rushcrew.queue.presentation.dto.response.CreatePolicyResponse;
 import jakarta.validation.Valid;
+import java.util.UUID;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,6 +39,27 @@ public class QueuePolicyController {
         CreatePolicyCommand command = request.toCommand();
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("TODO"));
     }
+
+    /**
+     * MASTER 권한만 가능
+     * 타임딜 정책 목록 조회 : 상품별, 상태별(RUNNING, PAUSED, STOPPED)
+     */
+    @GetMapping
+    public ResponseEntity<ApiResponse<Void>> getTimeDealPolicies(
+        @RequestParam(required = false) UUID productId,
+        @RequestParam(required = false) String status,
+        @RequestParam(name = "page", defaultValue = "1") int page,
+        @RequestParam(name = "size", defaultValue = "10") int size,
+        @RequestParam(name = "sort-by", defaultValue = "createdAt") String sortBy,
+        @RequestParam(name = "sort-direction", defaultValue = "DESC") Sort.Direction sortDirection
+    ) {
+
+        PageQuery pageQuery = PageQuery.of(page, size, sortBy, sortDirection);
+        SearchPolicyCommand command = SearchPolicyCommand.of(productId, QueuePolicyStatus.valueOf(status));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("TODO"));
+    }
+
+
 
 
 
