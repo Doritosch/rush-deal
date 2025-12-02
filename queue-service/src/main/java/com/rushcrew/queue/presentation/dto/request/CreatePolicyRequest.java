bb89a716f71fa6518c1d202484e80949940e173a
songkyung <min.songkyung@gmail.com>
2025-12-03 02:07:49 +0900

    fix: CreatePolicyRequest dto validation 에러 수정

--- queue-service/src/main/java/com/rushcrew/queue/presentation/dto/request/CreatePolicyRequest.java
+++ queue-service/src/main/java/com/rushcrew/queue/presentation/dto/request/CreatePolicyRequest.java
@@ -6,6 +6,7 @@ import com.rushcrew.queue.domain.vo.TimePeriod;
 import com.rushcrew.queue.domain.vo.TrafficSetting;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -13,23 +14,23 @@ public record CreatePolicyRequest(
     // TODO : 추후 추가 예정
 //    Long userId,
 //    UserRole role,
-    @NotBlank(message = "상품 ID는 필수입니다")
+    @NotNull(message = "상품 ID는 필수입니다")
     UUID productId,
     @NotBlank(message = "타임딜 제목은 필수입니다")
     String dealName,
-    @NotBlank(message = "초기 상태값은 필수입니다")
+    @NotNull(message = "초기 상태값은 필수입니다")
     QueuePolicyStatus status,
-    @NotBlank(message = "시작 시간은 필수입니다")
+    @NotNull(message = "시작 시간은 필수입니다")
     LocalDateTime startTime,
-    @NotBlank(message = "종료 시간은 필수입니다")
+    @NotNull(message = "종료 시간은 필수입니다")
     LocalDateTime endTime,
-    @NotBlank(message = "허용 인원은 필수입니다")
+    @NotNull(message = "허용 인원은 필수입니다")
     @Min(value = 1, message = "허용 인원은 최소 1명 이상이어야 합니다")
     Integer limitSize,
-    @NotBlank(message = "활성 체크 주기는 필수입니다")
+    @NotNull(message = "활성 체크 주기는 필수입니다")
     @Min(value = 1, message = "활성 체크 주기는 최소 1 이상이어야 합니다")
     Integer queueGap,
-    @NotBlank(message = "TTL은 필수입니다")
+    @NotNull(message = "TTL은 필수입니다")
     @Min(value = 60, message = "TTL은 최소 60초 이상이어야 합니다")
     Integer ttl
 ) {
