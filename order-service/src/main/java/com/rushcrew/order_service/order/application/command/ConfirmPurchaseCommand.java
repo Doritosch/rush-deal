f3e9a90f3d62a9e58190a5da39fc2f2a445ea0de
chachohee <cxoxee@gmail.com>
2025-12-04 01:36:39 +0900

    refact(order): 커맨드 record 구조로 변경

--- order-service/src/main/java/com/rushcrew/order_service/order/application/command/ConfirmPurchaseCommand.java
+++ order-service/src/main/java/com/rushcrew/order_service/order/application/command/ConfirmPurchaseCommand.java
@@ -2,14 +2,7 @@ package com.rushcrew.order_service.order.application.command;
 
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class ConfirmPurchaseCommand {
-	private UUID orderId;
-	private Long userId;
-}
+public record ConfirmPurchaseCommand(
+	UUID orderId,
+	Long userId
+) { }
