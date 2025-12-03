0f33201daf884c769e765b120da92ef81fe2541c
songkyung <min.songkyung@gmail.com>
2025-12-03 20:10:35 +0900

    feature: Queue Service 레이어 구현 및 대기열 진입 메서드 구현

--- queue-service/src/main/java/com/rushcrew/queue/infrastructure/repository/RedisQueueRepository.java
+++ queue-service/src/main/java/com/rushcrew/queue/infrastructure/repository/RedisQueueRepository.java
@@ -1,23 +1,36 @@
 package com.rushcrew.queue.infrastructure.repository;
 
 import com.rushcrew.queue.domain.entity.QueueToken;
-import com.rushcrew.queue.domain.repository.QueueTokenRepository;
+import com.rushcrew.queue.domain.repository.QueueRepository;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RedisQueueRepository implements QueueTokenRepository {
+public class RedisQueueRepository implements QueueRepository {
 
-    private final RedisTemplate<String, QueueToken> redisTemplate;
+    private final StringRedisTemplate redisTemplate;
 
-    public RedisQueueRepository(RedisTemplate<String, QueueToken> redisTemplate) {
+    private static final String WAITING_KEY = "queue:wait:product:%s";
+    private static final String ACTIVE_KEY = "queue:active:product:%s";
+
+    public RedisQueueRepository(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
+    /**
+     * 대기열 등록 (ZSet : Sorted Set)
+     */
     @Override
     public void register(QueueToken token) {
-
+        double score = System.currentTimeMillis();
+        redisTemplate.opsForZSet().add(
+                getWaitingKey(token.getProductId()),
+                token.getId().getValue().toString(),
+                score
+            );
     }
 
     @Override
@@ -26,12 +39,27 @@ public class RedisQueueRepository implements QueueTokenRepository {
     }
 
     @Override
-    public boolean isActivatedToken(Long productId, String token) {
-        return false;
+    public boolean isActivatedToken(Long productId, QueueToken token) {
+        return Boolean.TRUE.equals(redisTemplate.opsForSet()
+            .isMember(getActiveKey(token.getProductId()),
+                token.getId().getValue().toString()
+            ));
     }
 
     @Override
-    public Long getRank(Long productId, String token) {
-        return 0L;
+    public Long getWaitingRank(UUID productId, QueueToken token) {
+        // ZRANK key member
+        return redisTemplate.opsForZSet()
+            .rank(getWaitingKey(token.getProductId()),
+                token.getId().getValue().toString()
+            );
+    }
+
+    private String getWaitingKey(UUID productId) {
+        return String.format(WAITING_KEY, productId);
+    }
+
+    private String getActiveKey(UUID productId) {
+        return String.format(ACTIVE_KEY, productId);
     }
 }
