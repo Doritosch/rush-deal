cbe3e2f7e7bbf6b28e6526f0e5e93fa96b9d25f1
songkyung <min.songkyung@gmail.com>
2025-12-04 21:11:42 +0900

    fix: request validation 수정

--- queue-service/src/main/java/com/rushcrew/queue/presentation/dto/request/EnterQueueRequest.java
+++ queue-service/src/main/java/com/rushcrew/queue/presentation/dto/request/EnterQueueRequest.java
@@ -1,10 +1,10 @@
 package com.rushcrew.queue.presentation.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record EnterQueueRequest(
-    @NotBlank(message = "상품 ID는 필수입니다")
+    @NotNull(message = "상품 ID는 필수입니다")
     UUID productId
 ) {
 }
