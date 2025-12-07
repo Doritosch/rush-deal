f2d09aee15c50465916221529ce6912b41d323da
chachohee <cxoxee@gmail.com>
2025-12-08 01:14:59 +0900

    feat(order): 주문 생성 기능 개발: CreateORderService 구현

---
+++ order-service/src/main/java/com/rushcrew/order_service/application/saga/orchestrator/OrderCreationSagaOrchestrator.java
@@ -0,0 +1,9 @@
+package com.rushcrew.order_service.application.saga.orchestrator;
+
+import com.rushcrew.order_service.application.command.dto.command.CreateOrderCommand;
+import com.rushcrew.order_service.application.command.dto.result.CreateOrderResult;
+
+public class OrderCreationSagaOrchestrator {
+	public CreateOrderResult execute(CreateOrderCommand command) {
+	}
+}
