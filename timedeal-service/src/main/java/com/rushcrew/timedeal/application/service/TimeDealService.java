b1cd7306573f858a30969b0042fcc7e9c6057ecd
minahYu <mina010311@naver.com>
2025-12-10 03:41:42 +0900

    feat: 서비스단에 타임딜 정보 수정 로직 추가

--- timedeal-service/src/main/java/com/rushcrew/timedeal/application/service/TimeDealService.java
+++ timedeal-service/src/main/java/com/rushcrew/timedeal/application/service/TimeDealService.java
@@ -1,11 +1,15 @@
 package com.rushcrew.timedeal.application.service;
 
 import com.rushcrew.timedeal.application.command.CreateTimeDealCommand;
+import com.rushcrew.timedeal.application.command.UpdateTimeDealCommand;
+import com.rushcrew.timedeal.application.result.UpdateTimeDealResult;
 import java.util.UUID;
 
 public interface TimeDealService {
 
     UUID createTimeDeal(CreateTimeDealCommand command);
+
+    UpdateTimeDealResult updateTimeDeal(UUID timeDealId, UpdateTimeDealCommand command);
 
     void forceEndTimeDeal(UUID timeDealId);
 }
