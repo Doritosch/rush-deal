5d3f6144a65932fb1154ef169a82f5d85c7cadfa
minahYu <mina010311@naver.com>
2025-12-10 15:29:51 +0900

    feat: controller에 재고 수량 변경 메서드 추가

--- timedeal-service/src/main/java/com/rushcrew/timedeal/presentation/StockController.java
+++ timedeal-service/src/main/java/com/rushcrew/timedeal/presentation/StockController.java
@@ -1,14 +1,20 @@
 package com.rushcrew.timedeal.presentation;
 
 import com.rushcrew.timedeal.application.command.CreateStockCommand;
+import com.rushcrew.timedeal.application.command.UpdateStockCountCommand;
 import com.rushcrew.timedeal.application.result.CreateStockResult;
+import com.rushcrew.timedeal.application.result.UpdateStockCountResult;
 import com.rushcrew.timedeal.application.service.StockService;
 import com.rushcrew.timedeal.presentation.dto.request.CreateStockRequest;
+import com.rushcrew.timedeal.presentation.dto.request.UpdateStockCountRequest;
 import com.rushcrew.timedeal.presentation.dto.response.CreateStockResponse;
+import com.rushcrew.timedeal.presentation.dto.response.UpdateStockCountResponse;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +34,15 @@ public class StockController {
         CreateStockCommand command = request.toCommand();
         CreateStockResult result = stockService.createStock(command);
         return ResponseEntity.status(HttpStatus.CREATED).body(CreateStockResponse.from(result));
+    }
+
+    @PostMapping("/{stockId}/change")
+    public ResponseEntity<UpdateStockCountResponse> changeStockCount(
+        @PathVariable UUID stockId,
+        @RequestBody @Valid UpdateStockCountRequest request
+    ) {
+        UpdateStockCountCommand command = request.toCommand();
+        UpdateStockCountResult result = stockService.changeStockCount(stockId, command);
+        return ResponseEntity.ok(UpdateStockCountResponse.from(result));
     }
 }
