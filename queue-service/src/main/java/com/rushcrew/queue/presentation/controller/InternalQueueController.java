375bd74f4aed6e2935737bfa7eafb0dbd7a93737
songkyung <min.songkyung@gmail.com>
2025-12-11 12:15:10 +0900

    setting: kafka 설정

--- queue-service/src/main/java/com/rushcrew/queue/presentation/controller/InternalQueueController.java
+++ queue-service/src/main/java/com/rushcrew/queue/presentation/controller/InternalQueueController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/internal/queues")
+@RequestMapping("/api/v1/internal/queues")
 public class InternalQueueController {
     private final QueuePort queueService;
 
