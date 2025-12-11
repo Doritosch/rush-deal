c8d16787faea667eda31acec67893bff71d7ad85
bbangjae <thebyunjae@naver.com>
2025-12-11 14:45:51 +0900

    chore(auth): 공통 모듈 의존성 추가 및 scanBasePackages 설정

--- api-gateway/src/main/java/com/rushcrew/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/rushcrew/api_gateway/ApiGatewayApplication.java
@@ -2,14 +2,24 @@ package com.rushcrew.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
 @ConfigurationPropertiesScan
+@SpringBootApplication(
+    scanBasePackages = {
+        "com.rushcrew.api_gateway",
+        "com.rushcrew.common.exception",
+    },
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+    }
+)
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
