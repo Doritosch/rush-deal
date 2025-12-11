f30c99f50b59fb768e5c938ff46e2040b4ff6d31
chachohee <cxoxee@gmail.com>
2025-12-11 12:25:16 +0900

    refact(order): builder 접근 제어 private 제거

--- order-service/src/main/java/com/rushcrew/order_service/domain/model/order/Order.java
+++ order-service/src/main/java/com/rushcrew/order_service/domain/model/order/Order.java
@@ -33,7 +33,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder(access = AccessLevel.PRIVATE)
+@Builder
 public class Order extends BaseEntity {
 
 	@Id
