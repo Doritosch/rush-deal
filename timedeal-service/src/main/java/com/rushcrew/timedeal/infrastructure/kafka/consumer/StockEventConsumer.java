414d96908a96132832613451835af64647617b88
minahYu <mina010311@naver.com>
2025-12-11 15:23:13 +0900

    feat: Kafka 재고 복구 이벤트 consumer 추가

--- timedeal-service/src/main/java/com/rushcrew/timedeal/infrastructure/kafka/consumer/StockEventConsumer.java
+++ timedeal-service/src/main/java/com/rushcrew/timedeal/infrastructure/kafka/consumer/StockEventConsumer.java
@@ -2,11 +2,13 @@ package com.rushcrew.timedeal.infrastructure.kafka.consumer;
 
 import com.rushcrew.timedeal.application.command.ConfirmStockCommand;
 import com.rushcrew.timedeal.application.command.ReserveStockCommand;
+import com.rushcrew.timedeal.application.command.RestoreStockCommand;
 import com.rushcrew.timedeal.application.service.StockService;
 import com.rushcrew.timedeal.domain.vo.OrderId;
 import com.rushcrew.timedeal.domain.vo.Quantity;
 import com.rushcrew.timedeal.infrastructure.kafka.dto.StockConfirmEvent;
 import com.rushcrew.timedeal.infrastructure.kafka.dto.StockReserveEvent;
+import com.rushcrew.timedeal.infrastructure.kafka.dto.StockRestoreEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -39,4 +41,17 @@ public class StockEventConsumer {
         );
         stockService.confirmStock(command);
     }
+
+    // 주문 취소 -> 재고 복구
+    @KafkaListener(topics = "order.cancelled")
+    public void restore(StockRestoreEvent event) {
+        RestoreStockCommand command = new RestoreStockCommand(
+            event.stockId(),
+            Quantity.positive(event.quantity()),
+            OrderId.of(event.orderId()),
+            event.reason()
+        );
+        stockService.restoreStock(command);
+    }
+
 }
