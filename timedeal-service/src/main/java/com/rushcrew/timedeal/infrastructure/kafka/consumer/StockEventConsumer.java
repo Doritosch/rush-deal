916420d1f5e8b9af507a3eb1f2e514082e115337
minahYu <mina010311@naver.com>
2025-12-11 14:33:56 +0900

    feat: Kafka 재고 확정 이벤트 consumer 추가

--- timedeal-service/src/main/java/com/rushcrew/timedeal/infrastructure/kafka/consumer/StockEventConsumer.java
+++ timedeal-service/src/main/java/com/rushcrew/timedeal/infrastructure/kafka/consumer/StockEventConsumer.java
@@ -1,9 +1,11 @@
 package com.rushcrew.timedeal.infrastructure.kafka.consumer;
 
+import com.rushcrew.timedeal.application.command.ConfirmStockCommand;
 import com.rushcrew.timedeal.application.command.ReserveStockCommand;
 import com.rushcrew.timedeal.application.service.StockService;
 import com.rushcrew.timedeal.domain.vo.OrderId;
 import com.rushcrew.timedeal.domain.vo.Quantity;
+import com.rushcrew.timedeal.infrastructure.kafka.dto.StockConfirmEvent;
 import com.rushcrew.timedeal.infrastructure.kafka.dto.StockReserveEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -25,5 +27,16 @@ public class StockEventConsumer {
             event.userId()
         );
         stockService.reserveStock(command);
+    }
+
+    // 결제 -> 재고 확정
+    @KafkaListener(topics = "payment.completed")
+    public void confirm(StockConfirmEvent event) {
+        ConfirmStockCommand command = new ConfirmStockCommand(
+            OrderId.of(event.orderId()),
+            event.stockId(),
+            Quantity.positive(event.quantity())
+        );
+        stockService.confirmStock(command);
     }
 }
