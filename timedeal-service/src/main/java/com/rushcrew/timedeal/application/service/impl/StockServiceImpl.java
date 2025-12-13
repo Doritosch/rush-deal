62ae01cf86da7e70e4939aca36aa84a874145bfd
minahYu <mina010311@naver.com>
2025-12-14 01:46:14 +0900

    refac: 주문 수량 검증 로직을 StockLog로 이동

--- timedeal-service/src/main/java/com/rushcrew/timedeal/application/service/impl/StockServiceImpl.java
+++ timedeal-service/src/main/java/com/rushcrew/timedeal/application/service/impl/StockServiceImpl.java
@@ -20,10 +20,8 @@ import com.rushcrew.timedeal.domain.port.StockCache;
 import com.rushcrew.timedeal.domain.repository.StockRepository;
 import com.rushcrew.timedeal.domain.repository.TimeDealRepository;
 import com.rushcrew.timedeal.domain.vo.OrderId;
-import com.rushcrew.timedeal.domain.vo.Quantity;
 import com.rushcrew.timedeal.domain.vo.TimeDealProductStatus;
 import com.rushcrew.timedeal.domain.vo.TimeDealStockStatus;
-import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -137,7 +135,9 @@ public class StockServiceImpl implements StockService {
         UUID orderId = command.orderId().getOrderId();
         TimeDealStock stock = getStockOrThrow(command.stockId());
         StockLog log = getLastLogOrThrow(command.stockId(), orderId);
-        validateOrderQuantity(log, command.quantity());
+
+        // 주문 당시 기록된 수량과 현재 처리하려는 수량이 동일한지 검증
+        log.validateOrderQuantity(command.quantity());
 
         stock.confirm(OrderId.of(orderId), command.quantity());
 
@@ -154,12 +154,6 @@ public class StockServiceImpl implements StockService {
     private StockLog getLastLogOrThrow(UUID stockId, UUID orderId) {
         return stockRepository.findLastByStockIdAndOrderId(stockId, orderId)
             .orElseThrow(() -> new BusinessException(TimeDealErrorCode.NOT_FOUND_ORDER));
-    }
-
-    private void validateOrderQuantity(StockLog log, Quantity quantity) {
-        if (!Objects.equals(log.getQuantity().getQuantity(), quantity.getQuantity())) {
-            throw new BusinessException(TimeDealErrorCode.INVALID_ORDER_INFO);
-        }
     }
 
     private void validQuantity(TimeDealStock stock, Long quantity) {
