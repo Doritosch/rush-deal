1944e51abde0b1ae48791430dc53e5c6cb36f9cf
chachohee <cxoxee@gmail.com>
2025-12-14 19:09:52 +0900

    refact(order): 주문 리팩토링 - 큐 변경

--- queue-service/src/main/java/com/rushcrew/queue/presentation/controller/InternalQueueController.java
+++ queue-service/src/main/java/com/rushcrew/queue/presentation/controller/InternalQueueController.java
@@ -5,9 +5,7 @@ import com.rushcrew.queue.application.port.in.QueuePort;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +37,7 @@ public class InternalQueueController {
         @RequestHeader(USER_ID_HEADER) Long currUserId,
         @RequestHeader(USER_ROLE_HEADER) String role
     ) {
-        boolean result = queueService.validateActivatedQueueToken(productId, queueToken, currUserId, role);
+        boolean result = queueService.validateActivatedQueueToken(productId, queueToken);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(result));
     }
 }
