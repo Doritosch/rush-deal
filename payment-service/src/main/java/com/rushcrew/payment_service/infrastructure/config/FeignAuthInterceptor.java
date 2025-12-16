121dd69b23313e8de0883e93aed569c9b7b5951f
Doritosch <als000825@naver.com>
2025-12-17 05:03:01 +0900

    refactor(payment): feignInterceptor 로직 수정

--- payment-service/src/main/java/com/rushcrew/payment_service/infrastructure/config/FeignAuthInterceptor.java
+++ payment-service/src/main/java/com/rushcrew/payment_service/infrastructure/config/FeignAuthInterceptor.java
@@ -1,5 +1,6 @@
 package com.rushcrew.payment_service.infrastructure.config;
 
+import com.rushcrew.payment_service.global.security.model.UserDetailsImpl;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.security.core.Authentication;
@@ -12,9 +13,10 @@ public class FeignAuthInterceptor implements RequestInterceptor {
     public void apply(RequestTemplate requestTemplate) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if(authentication != null && authentication.getCredentials() != null) {
-            String token = authentication.getCredentials().toString();
-            requestTemplate.header("Authorization", "Bearer " + token);
+        if(authentication != null && authentication.getPrincipal() instanceof UserDetailsImpl userDetails) {
+            requestTemplate.header("X-User-Id", String.valueOf(userDetails.userId()));
+            requestTemplate.header("X-User-Email", userDetails.email());
+            requestTemplate.header("X-User-Role", userDetails.role());
         }
     }
 }
