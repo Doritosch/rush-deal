f644261b85c0382ae2d1b2fbae3900411a040b0a
minahYu <mina010311@naver.com>
2025-12-18 12:14:57 +0900

    feat: Product 관련 controller에 권한 검증 로직 추가

--- product-service/src/main/java/com/rushcrew/product/presentation/ProductController.java
+++ product-service/src/main/java/com/rushcrew/product/presentation/ProductController.java
@@ -8,6 +8,7 @@ import com.rushcrew.product.application.result.ProductDetailResult;
 import com.rushcrew.product.application.result.ProductResult;
 import com.rushcrew.product.application.result.UpdateProductResult;
 import com.rushcrew.product.application.service.ProductService;
+import com.rushcrew.product.global.security.model.UserDetailsImpl;
 import com.rushcrew.product.presentation.dto.request.CreateProductRequest;
 import com.rushcrew.product.presentation.dto.request.UpdateProductRequest;
 import com.rushcrew.product.presentation.dto.response.CreateProductResponse;
@@ -24,6 +25,8 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -42,45 +45,57 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('MASTER', 'SELLER')")
     public ResponseEntity<CreateProductResponse> createProduct(
-        @Valid @RequestBody CreateProductRequest request
+        @Valid @RequestBody CreateProductRequest request,
+        @AuthenticationPrincipal UserDetailsImpl principal
     ) {
-        CreateProductCommand command = CreateProductCommand.from(request);
-        CreateProductResult result = productService.createProduct(command);
+        CreateProductCommand command = request.toCommand();
+        CreateProductResult result =
+            productService.createProduct(principal.userId(), principal.role(), command);
         return ResponseEntity.status(HttpStatus.CREATED).body(CreateProductResponse.from(result));
     }
 
     @PatchMapping("/{productId}")
+    @PreAuthorize("hasAnyRole('MASTER', 'SELLER')")
     public ResponseEntity<UpdateProductResponse> updateProduct(
         @PathVariable UUID productId,
-        @Valid @RequestBody UpdateProductRequest request
+        @Valid @RequestBody UpdateProductRequest request,
+        @AuthenticationPrincipal UserDetailsImpl principal
     ) {
-        UpdateProductCommand command = UpdateProductCommand.from(request);
-        UpdateProductResult result = productService.updateProduct(productId, command);
+        UpdateProductCommand command = request.toCommand();
+        UpdateProductResult result =
+            productService.updateProduct(principal.userId(), principal.role(), productId, command);
         return ResponseEntity.ok(UpdateProductResponse.from(result));
     }
 
     @PostMapping("/{productId}/disable")
+    @PreAuthorize("hasAnyRole('MASTER', 'SELLER')")
     public ResponseEntity<Void> disableProduct(
-        @PathVariable UUID productId
+        @PathVariable UUID productId,
+        @AuthenticationPrincipal UserDetailsImpl principal
     ) {
-        productService.disableProduct(productId);
+        productService.disableProduct(principal.userId(), principal.role(), productId);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{productId}/enable")
+    @PreAuthorize("hasAnyRole('MASTER', 'SELLER')")
     public ResponseEntity<Void> enableProduct(
-        @PathVariable UUID productId
+        @PathVariable UUID productId,
+        @AuthenticationPrincipal UserDetailsImpl principal
     ) {
-        productService.enableProduct(productId);
+        productService.enableProduct(principal.userId(), principal.role(), productId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{productId}")
+    @PreAuthorize("hasAnyRole('MASTER', 'SELLER')")
     public ResponseEntity<Void> deleteProduct(
-        @PathVariable UUID productId
+        @PathVariable UUID productId,
+        @AuthenticationPrincipal UserDetailsImpl principal
     ) {
-        productService.deleteProduct(productId);
+        productService.deleteProduct(principal.userId(), principal.role(), productId);
         return ResponseEntity.noContent().build();
     }
 
