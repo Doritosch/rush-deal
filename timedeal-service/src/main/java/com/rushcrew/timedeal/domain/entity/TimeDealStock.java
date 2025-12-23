b7cf0b9324aef67bdb66c7d2e4b4f77a2598bd30
minahYu <mina010311@naver.com>
2025-12-23 15:42:46 +0900

    refactor: discountPrice를 TimeDeal entity에만 두고 fetch join으로 조회 방식 수정

--- timedeal-service/src/main/java/com/rushcrew/timedeal/domain/entity/TimeDealStock.java
+++ timedeal-service/src/main/java/com/rushcrew/timedeal/domain/entity/TimeDealStock.java
@@ -29,9 +29,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
-
-import java.math.BigDecimal;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -75,10 +72,6 @@ public class TimeDealStock extends BaseEntity {
     })
     private StockCounts stockCounts;
 
-	// 할인된 가격 추가
-	@Column(name = "discount_price", precision = 10, scale = 2)
-	private BigDecimal discountPrice;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TimeDealStockStatus status;
@@ -91,7 +84,6 @@ public class TimeDealStock extends BaseEntity {
     @Builder.Default
     private List<StockLog> stockLogs = new ArrayList<>();
 
-	// TODO: 할인 가격 저장
     public static TimeDealStock create(CreateStockCommand command,
         TimeDealProduct timeDealProduct) {
         TimeDealStock stock = TimeDealStock.builder()
